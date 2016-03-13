f83596959f8dc48dbc1745f77887e16395df1342
Fabio Berger <fabioberger1991@gmail.com>
2016-03-13 12:39:37 -0700

    Fix issues caught by eslint

--- public/js/redux/thunks/items_thunks.js
+++ public/js/redux/thunks/items_thunks.js
@@ -2,10 +2,10 @@ var itemsActionCreators = require('redux/actions/items_action_creators');
 var requestor = require('redux/requestor');
 
 var itemsThunks = {
-    fetchItems: workspaceId => {
+    fetchItems: () => {
         return async dispatch => {
             dispatch(itemsActionCreators.requestItems());
-            var response = await requestor.getRequestAsync(`/items`);
+            var response = await requestor.getRequestAsync('/items');
             if (response.error) {
                 dispatch(itemsActionCreators.requestItemsFailed(response.error));
             } else {
