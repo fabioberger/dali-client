56cef43f25471c64a0d172086601dd1c5b90394d
Fabio Berger <fabioberger1991@gmail.com>
2016-03-20 00:56:20 -0700

    Add some paintings to store initial state, set up provider and make gallery component connect to store and display items

--- js/components/gallery.js
+++ js/components/gallery.js
@@ -1,6 +1,16 @@
 import React from 'react';
+const ReactRedux = require('react-redux');
 
 const Gallery = React.createClass({
+    propTypes: {
+        items: React.PropTypes.arrayOf(
+            React.PropTypes.shape({
+                name: React.PropTypes.string.isRequired,
+                description: React.PropTypes.string.isRequired,
+                url_image: React.PropTypes.string.isRequired,
+            })
+        ).isRequired,
+    },
     _renderItem: function(item) {
         return (
             <div className="item">
@@ -13,21 +23,18 @@ const Gallery = React.createClass({
         );
     },
     render: function() {
-        const item = {
-            'id': 1,
-            'created_date': '2016-02-01T00:00:00Z',
-            'name': 'The Persistence of Memory',
-            'description': 'By Salvador Dali (1931)',
-            'slug': 'the-persistence-of-memory',
-            'source': 'custom',
-            'url_image': 'http://uploads5.wikiart.org/images/salvador-dali/the-persistence-of-memory-1931.jpg',
-        };
         return (
             <div className="gallery">
-                {this._renderItem(item)}
+                {this._renderItem(this.props.items[0])}
             </div>
         );
     },
 });
 
-module.exports = Gallery;
+function mapStateToProps(state) {
+    return {
+        items: state.items.items,
+    };
+}
+
+module.exports = ReactRedux.connect(mapStateToProps)(Gallery);
--- js/redux/initial_states/items_initial_state.js
+++ js/redux/initial_states/items_initial_state.js
@@ -1,6 +1,25 @@
 const itemsInitialState = {
     isFetching: false,
-    items: [],
+    items: [
+        {
+            'id': 1,
+            'created_date': '2016-02-01T00:00:00Z',
+            'name': 'The Persistence of Memory',
+            'description': 'By Salvador Dali (1931)',
+            'slug': 'the-persistence-of-memory',
+            'source': 'custom',
+            'url_image': 'http://uploads5.wikiart.org/images/salvador-dali/the-persistence-of-memory-1931.jpg',
+        },
+        {
+            'id': 2,
+            'created_date': '2016-02-01T00:00:00Z',
+            'name': 'Ballerina in a Deaths Head',
+            'description': 'By Salvador Dali (1939)',
+            'slug': 'ballerina-in-a-deaths-head',
+            'source': 'custom',
+            'url_image': 'http://uploads2.wikiart.org/images/salvador-dali/ballerina-in-a-death-s-head.jpg',
+        },
+    ],
 };
 
 module.exports = itemsInitialState;
